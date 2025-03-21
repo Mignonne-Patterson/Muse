0aafb1530f1244cdee588ef2f94ebf68eb79b18a
Mignonne Patterson <thefunnyreminder15@proton.me>
2025-03-21 06:15:05 +0000

    Update utilities.lisp

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -1,8 +1,15 @@
-```lisp
-defpackage :muse.utilities
-  (:nicknames mu)
+(defpackage :muse.utilities
+  (:nicknames :mu)
   (:export #:db-to-linear
-           ;; Other utility functions will be exported here...)) 
-in-package muse:utilities 
-implementations for various common utilities used in the project # db to linear conversion (for sound processing) defun-db->linear(db-level lin-range=1.0)(if (< -87.6 db- level)-inf(* 2 (/ (- lin-range)(* 3495.7 ln(-1(/ (* .04 (.01 (+ (expt *e* /* log(10)))))db-level) 
-```}<|im_start|>
+           ;; Other utility functions will be exported here...
+           ))
+
+(in-package :muse.utilities)
+
+;; Implementations for various common utilities used in the project
+
+;; dB to linear conversion (for sound processing)
+(defun db->linear (db-level &optional (lin-range 1.0))
+  (if (< db-level -87.6)
+      -inf
+      (* lin-range (expt 10 (/ db-level 20)))))
